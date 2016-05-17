d0055400199d262286605b49592acea4997166cb
eldooon <eldonchan91@gmail.com>
2016-05-17 14:04:54 -0400

    Done.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,46 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+-(NSArray *) namesForIceCream:(NSString *)iceCream;{
+    
+    NSMutableArray *iceCreamArray = [[NSMutableArray alloc]init]; //create new mutable array
+    NSDictionary *iceCreamName = @ {    @"Joe"    : @"Peanut Butter and Chocolate",
+                                        @"Tim"    : @"Natural Vanilla",
+                                        @"Sophie" : @"Mexican Chocolate",
+                                        @"Deniz"  : @"Natural Vanilla",
+                                        @"Tom"    : @"Mexican Chocolate",
+                                        @"Jim"    : @"Natural Vanilla",
+                                        @"Mark"   : @"Cookies 'n Cream"};
+    
+    for (NSString *keys in iceCreamName){ //create new "keys" string for loop in dictionary
+        if ([iceCreamName[keys] isEqualToString:iceCream]){ //if any values is equal to iceCream string
+            
+            [iceCreamArray addObject: keys]; //add keys to array
+        }
+            
+    }
+    
+
+    return iceCreamArray; //return new array with keys
+}
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName{
+    
+    NSMutableDictionary *countsByIceCream = [[NSMutableDictionary alloc]init]; //create new mutable dictionary
+    
+    for (NSString *keys in iceCreamByName){
+        NSString *iceCream = iceCreamByName[keys];
+        
+        if ([countsByIceCream objectForKey:iceCream] == 0){
+            
+            NSArray *countingIceCream = [self namesForIceCream:(iceCream)]; //create new array using previous method
+            NSNumber *countPerIceCream = @([countingIceCream count]); //intialize count per icecream
+            countsByIceCream[iceCream] = countPerIceCream; //all together
+        }
+    }
+    
+    return countsByIceCream; //return new dictionary
+}
+
 
 @end
